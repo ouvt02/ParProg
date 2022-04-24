2d881f8aa136ffda3ce9c8b99d07c11696f358bc
ouvt02 <ouvt02@gmail.com>
2022-04-24 22:38:06 +0300

    fix

--- MPI/lab1/lab1_mpi.cpp
+++ MPI/lab1/lab1_mpi.cpp
@@ -6,7 +6,6 @@
 
 #define TAG_PREV 5
 #define TAG_SOLVED 6
-#define TAG_WRITTED 7
 
 const int TIME = 10;
 const int SIZE = 10;
@@ -29,58 +28,58 @@ double psi(double t)
 }
 
 int main(int argc, char** argv)
-{    
+{
     int size = 0;
     int rank = 0;
-    
+
     MPI_Status status;
-    
+
     MPI_Init(&argc, &argv);
-    
+
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     clock_t start_time = clock();
-        
+
     int start_num = rank * (int)(SIZE / H) / size;
     int stop_num = (rank + 1) * (int)(SIZE / H) / size - 1;
-    
+
     int cur_size = stop_num - start_num + 1;
     if (rank != 0)
         cur_size += 1;
-           
+
     double** u_cur = new double* [(int)(TIME / TAU)];
-    
+
     for (int i = 0; i < (int)(TIME / TAU); i++)
     {
         u_cur[i] = new double[cur_size];
         if (i == 0)
-        {       
+        {
             for (int j = 0; j < cur_size; j++)
                 u_cur[0][j] = (rank == 0) ? phi(H * (j + start_num)) : phi(H * (j + start_num - 1));
         }
-        
+
         else
         {
             if (rank == 0)
             u_cur[i][0] = psi(i * TAU);
-        
+
             else
                 MPI_Recv(&(u_cur[i][0]), 1, MPI_DOUBLE, rank - 1, TAG_PREV, MPI_COMM_WORLD, &status);
-            
+
             for (int j = 1; j < cur_size; j++)
             {
-                u_cur[i][j] = (2 * f(H * (j + 1/2), TAU * (i + 1/2)) - 
+                u_cur[i][j] = (2 * f(H * (j + 1/2), TAU * (i + 1/2)) -
                     (1/TAU - 1/H) * u_cur[i][j - 1] + (1/TAU + 1/H) * u_cur[i - 1][j - 1]
                     + (1/TAU - 1/H) * u_cur[i - 1][j])/(1/TAU + 1/H);
             }
-            
+
             if (rank != size - 1)
                 MPI_Send(&(u_cur[i][cur_size - 1]), 1, MPI_DOUBLE, rank + 1, TAG_PREV, MPI_COMM_WORLD);
         }
     }
-    
-    
+
+
     if (rank == 0)
     {
         double** u = new double* [(int)(TIME / TAU)];
@@ -89,9 +88,9 @@ int main(int argc, char** argv)
         {
             u[i] = new double [(int)(SIZE / H)];
             memcpy(u[i], u_cur[i], cur_size * sizeof(double));
-            
+
         }
-        
+
         for (int r = 1; r < size; r++)
         {
             int start_num_r = r * (int)(SIZE / H) / size;
@@ -101,34 +100,34 @@ int main(int argc, char** argv)
             for (int i = 0; i < (int)(TIME / TAU); i++)
                 MPI_Recv(u[i] + start_num_r, cur_size_r - 1, MPI_DOUBLE, r, TAG_SOLVED, MPI_COMM_WORLD, &status);
         }
-        
+
         clock_t stop_time = clock();
         printf("Completed in %ld\n", stop_time - start_time);
-        
+
         FILE* fp = nullptr;
-        
+
         fp = fopen("solution.txt", rank == 0 ? "w" : "a");
-        
+
         for (int j = 0; j < (int)(SIZE / H); j++)
         {
           for (int i = 0; i < (int)(TIME / TAU); i++)
             fprintf(fp, "%lg ", u[i][j]);
-          
+
           fprintf(fp, "\n");
         }
-        
+
         fclose(fp);
     }
-    
+
     else
     {
         for (int i = 0; i < (int)(TIME / TAU); i++)
             MPI_Send(u_cur[i] + 1, cur_size - 1, MPI_DOUBLE, 0, TAG_SOLVED, MPI_COMM_WORLD);
     }
-    
-    
+
+
 
     MPI_Finalize();
-    
+
     return 0;
 }

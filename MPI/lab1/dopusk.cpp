bd753040d130dac98c7720872e56599ffa5134b5
ouvt02 <ouvt02@gmail.com>
2022-05-04 17:50:46 +0300

    Another function for getting time

--- MPI/lab1/dopusk.cpp
+++ MPI/lab1/dopusk.cpp
@@ -4,6 +4,14 @@
 #include <time.h>
 #define TAG 5
 
+long int clk()
+{
+  timespec time;
+  clock_gettime(CLOCK_MONOTONIC, &time);
+  long int time_ns = time.tv_sec * 1000000000 + time.tv_nsec;
+  return time_ns;
+}
+
 int main(int argc, char** argv)
 {    
     int size = 0;
@@ -22,19 +30,24 @@ int main(int argc, char** argv)
     
     if (rank == 0)
     {
-        start_time = clock();
-        MPI_Send(&N, 1, MPI_INT, rank + 1, TAG, MPI_COMM_WORLD);
-        MPI_Recv(&N, 1, MPI_INT, size - 1, TAG, MPI_COMM_WORLD, &status);
-        finish_time = clock();
-        printf("TIME = %ld\n ", (finish_time - start_time)/size);
+        start_time = clk();
+        for (int i = 0; i < 100000; i++)
+        {
+            MPI_Send(&N, 1, MPI_INT, rank + 1, TAG, MPI_COMM_WORLD);
+            MPI_Recv(&N, 1, MPI_INT, size - 1, TAG, MPI_COMM_WORLD, &status);
+        }
+
+        finish_time = clk();
+        printf("TIME = %f\n ", (double)(finish_time - start_time)/(100000 * size));
     }
     
     else
     {
-        
-        MPI_Recv(&N, 1, MPI_INT, rank - 1, TAG, MPI_COMM_WORLD, &status);
-        MPI_Send(&N, 1, MPI_INT, (rank + 1)%size, TAG, MPI_COMM_WORLD);
-        
+        for (int i = 0; i < 100000; i++)
+        {
+            MPI_Recv(&N, 1, MPI_INT, rank - 1, TAG, MPI_COMM_WORLD, &status);
+            MPI_Send(&N, 1, MPI_INT, (rank + 1)%size, TAG, MPI_COMM_WORLD);
+        }
     }
     
     MPI_Finalize();    
